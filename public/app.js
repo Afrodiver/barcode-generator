23f7b0a4d8209d10c2e17acafe96825da08a96b3
Afrodiver <47467791+Afrodiver@users.noreply.github.com>
2026-07-24 22:01:28 -0500

    Implement barcode generation and download functions
    Added functions to generate and download barcodes based on user input.

--- public/app.js
+++ public/app.js
@@ -1,1 +1,39 @@
+function generate(){
 
+    let type =
+        document.getElementById("type").value;
+
+    let text =
+        document.getElementById("text").value;
+
+    let scale =
+        document.getElementById("scale").value;
+
+
+    let url =
+        `/barcode?type=${type}&text=${encodeURIComponent(text)}&scale=${scale}`;
+
+
+    document.getElementById("barcode").src=url;
+
+}
+
+
+
+function download(format){
+
+    let type =
+        document.getElementById("type").value;
+
+    let text =
+        document.getElementById("text").value;
+
+    let scale =
+        document.getElementById("scale").value;
+
+
+    window.open(
+        `/barcode?type=${type}&text=${encodeURIComponent(text)}&scale=${scale}&format=${format}`
+    );
+
+}
